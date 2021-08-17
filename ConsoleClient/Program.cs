9a9cee1728661ed6e9282ed5b71576a0d4fdb6bb
ABel Batista Rodríguez <AbelBatiista@outlook.com>
2021-08-17 14:55:18 -0400

    Add project files.

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string _url = @"http://localhost:5000/serverHub";
+            var connection = new HubConnectionBuilder()
+                .WithUrl(_url)
+                .Build();
+
+            connection.StartAsync().Wait();
+            connection.InvokeCoreAsync("SendSome", args: new[] { "Hello" });
+            connection.On("ReceiveSome", (string item) =>
+            {
+                Console.WriteLine(item);
+            });
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Server/ServerHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class ServerHub : Hub
+    {
+
+        public async Task SendSome(List<string> items)
+        {
+            await Clients.All.SendAsync("ReceiveSome", items);
+        }
+
+    }
+}
